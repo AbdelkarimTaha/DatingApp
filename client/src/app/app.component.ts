a587c16e5d153e6806050eee400024ac4e433d60
Karim Taha <abdelkareemtaha2014@gmail.com>
2021-09-25 23:11:46 +0200

    Add Register component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
@@ -10,13 +9,10 @@ import { AccountService } from './_services/account.service';
 })
 export class AppComponent implements OnInit{
   title = 'client';
-  users: any;
 
-
-constructor(private http: HttpClient, private accountService: AccountService){}
+constructor(private accountService: AccountService){}
 
   ngOnInit(){
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -25,11 +21,4 @@ constructor(private http: HttpClient, private accountService: AccountService){}
     this.accountService.setCurrentUser(user);
   }
 
-  getUsers(){
-    this.http.get('https://localhost:44397/api/Users').subscribe(response => {
-      this.users = response
-    }, error => {
-      console.log(error);
-    })
-  }
 }
