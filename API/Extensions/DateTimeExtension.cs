a0bc78e787ce1b2c877403240715749e17908166
Karim Taha <abdelkareemtaha2014@gmail.com>
2021-10-23 02:31:29 +0200

    Calculate Age by using date time extension method

---
+++ API/Extensions/DateTimeExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Extensions
+{
+    public static class DateTimeExtension
+    {
+        public static int CalculateAge(this DateTime dob)
+        {
+            var age = DateTime.Today.Year - dob.Year;
+
+            return (dob.Date > DateTime.Today.AddYears(-age)) ? --age : age;
+        }
+    }
+}
