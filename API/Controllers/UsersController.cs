1ed77afe36f2ee20ed4eb2fc7e4841f4810488a4
Karim Taha <abdelkareemtaha2014@gmail.com>
2021-10-26 00:08:19 +0200

    Auto Mapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 ﻿using API.Data;
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,23 +16,31 @@ namespace API.Controllers
     public class UsersController : BaseApiController
     {
         public IUserRepository _userRepository { get; }
+        public IMapper _mapper { get; }
 
-        public UsersController(IUserRepository userRepository)
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
             _userRepository = userRepository;
+            _mapper = mapper;
         }
 
         [HttpGet]
         [AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            return Ok(await _userRepository.GetUsersAsync());
+            var users = await _userRepository.GetUsersAsync();
+
+            var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
+
+            return Ok(usersToReturn);
         }
 
         [HttpGet("{username}")]
-        public async Task<ActionResult<AppUser>> GetUser(string username)
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
-            return Ok(await _userRepository.GetUserByUserNameAsync(username));
+            var user = await _userRepository.GetUserByUserNameAsync(username);
+
+            return _mapper.Map<MemberDto>(user);
         }
     }
 }
---
+++ API/Helper/AutoMapperProfiles.cs
@@ -0,0 +1,22 @@
+﻿using API.DTOs;
+using API.Entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Helper
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(des => des.PhotoUrl, opt => opt.MapFrom(src =>
+                src.Photos.FirstOrDefault(x => x.IsMain).Url));
+
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
